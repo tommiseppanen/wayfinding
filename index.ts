3a8577b87c00c7b1cf1b07375a893fced680a8f4
Tommi Seppänen <tommi.t.seppanen@gmail.com>
2022-09-05 17:03:27 +0300

    Started to implement breadthFirstSearch for path finding

--- index.ts
+++ index.ts
@@ -75,6 +75,49 @@ declare global {
   }
 }
 
+type Node = {
+  id: number;
+  lat: number;
+  lng: number;
+  connections: number[];
+  path: Node[];
+};
+
+//For more complex graph we could use A* search
+function breadthFirstSearch(graph: Node[], start: Node, idToSearch: number) {
+  let queue : Node[] = [];
+  let path : Node[] = [];
+  queue.push(start);
+  path.push(start);
+  let visited : number[] = [];
+  visited.push(start.id);
+
+  while (queue.length > 0) {
+      let node = queue.shift();
+
+      if (node === undefined)
+        return null;
+
+      for (var i = 0; i < node.connections.length; i++) {
+          let connectedNode = graph.find(e => e.id === node!.connections[i]);
+          if (connectedNode === undefined)
+            continue;
+
+          connectedNode.path = [...node.path];
+          connectedNode.path.push(connectedNode)
+          if (!visited.includes(connectedNode.id)) {
+              
+              if (connectedNode.id === idToSearch)
+                return connectedNode.path;
+              
+              visited.push(connectedNode.id);
+              queue.push(connectedNode);
+          }
+      }
+  }
+  return null;
+};
+
 window.initMap = initMap;
 export {};
   
