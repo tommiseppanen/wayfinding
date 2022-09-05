ae3c5c2ec757cf2b718ab4839e6fc8a010605c45
Tommi Seppänen <tommi.t.seppanen@gmail.com>
2022-09-05 18:15:44 +0300

    Find the path

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
+import nodes from "./nodes";
+
 function initMap(): void {
-  const map = new google.maps.Map(
+
+  window.mapInstance = new google.maps.Map(
       document.getElementById("map") as HTMLElement,
       {
       center: { lat: 60.31689593036639, lng: 24.96863419739257 },
@@ -8,52 +11,7 @@ function initMap(): void {
       }
   );
 
-  const markerSymbol = {
-      path: google.maps.SymbolPath.CIRCLE,
-      scale: 8,
-      strokeColor: "#ff0",
-  };
-
-  const endSymbol = {
-      path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
-      strokeColor: "#0f0",
-  };
-
-  // Create the polyline and add the symbol to it via the 'icons' property.
-  const line = new google.maps.Polyline({
-      path: [
-      { lat: 60.315331190208546, lng: 24.972571565188332},
-      { lat: 60.315714664255545, lng: 24.972182644881173},
-      { lat: 60.31610188791155, lng: 24.971790430997714},
-      { lat: 60.316494737521516, lng: 24.971553683280945},
-      { lat: 60.31685279758621, lng: 24.970946817488766},
-      { lat: 60.317219256810795, lng: 24.9705817558243},
-      { lat: 60.31762575716601, lng: 24.970165919930253},
-      { lat: 60.3180011176533, lng: 24.969774123293575},
-      { lat: 60.31840340209919, lng: 24.969358490636353},
-      { lat: 60.31880251327395, lng: 24.969025745640693},
-      { lat: 60.3192163715269, lng: 24.968563656747335},
-      { lat: 60.31942754545799, lng: 24.968359808862203},
-      { lat: 60.31954043662202, lng: 24.968255202710623},
-      { lat: 60.31947668636559, lng: 24.96808354133367},
-      { lat: 60.319205746387354, lng: 24.967549781739706},
-      { lat: 60.318872550265866, lng: 24.966784371016786},
-      ],
-      strokeColor: "#0f0",
-      icons: [
-      {
-          icon: markerSymbol,
-          offset: "100%",
-      }, 
-      {
-          icon: endSymbol,
-          offset: "100%",
-      },
-      ],
-      map: map,
-  });
-
-  animateCircle(line);
+  drawPath(21, 15);
 }
   
 function animateCircle(line: google.maps.Polyline) {
@@ -72,6 +30,7 @@ function animateCircle(line: google.maps.Polyline) {
 declare global {
   interface Window {
     initMap: () => void;
+    mapInstance: google.maps.Map;
   }
 }
 
@@ -80,13 +39,52 @@ type Node = {
   lat: number;
   lng: number;
   connections: number[];
-  path: Node[];
+  path?: Node[];
 };
+
+function drawPath(start: number, end: number) {
+  const markerSymbol = {
+    path: google.maps.SymbolPath.CIRCLE,
+    scale: 8,
+    strokeColor: "#ff0",
+  };
+
+  const endSymbol = {
+      path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+      strokeColor: "#0f0",
+  };
+
+  // Create the polyline and add the symbol to it via the 'icons' property.
+  const line = new google.maps.Polyline({
+    path: getNodes(start, end),
+    strokeColor: "#0f0",
+    icons: [
+    {
+        icon: markerSymbol,
+        offset: "100%",
+    }, 
+    {
+        icon: endSymbol,
+        offset: "100%",
+    },
+    ],
+    map: window.mapInstance,
+  });
+
+  animateCircle(line);
+}
+
+function getNodes(start: number, end: number) {
+  const startNode = nodes.find(e => e.id === start);
+  if (startNode)
+    return breadthFirstSearch(nodes, startNode, end);
+}
 
 //For more complex graph we could use A* search
 function breadthFirstSearch(graph: Node[], start: Node, idToSearch: number) {
   let queue : Node[] = [];
   let path : Node[] = [];
+  start.path = new Array(start);
   queue.push(start);
   path.push(start);
   let visited : number[] = [];
@@ -103,7 +101,7 @@ function breadthFirstSearch(graph: Node[], start: Node, idToSearch: number) {
           if (connectedNode === undefined)
             continue;
 
-          connectedNode.path = [...node.path];
+          connectedNode.path = node.path === undefined ? [] : [...node.path];
           connectedNode.path.push(connectedNode)
           if (!visited.includes(connectedNode.id)) {
               
