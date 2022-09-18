6baa38a29646bbc85092693abb680fc840b91899
Tommi Seppänen <tommi.t.seppanen@gmail.com>
2022-09-18 14:57:57 +0300

    Address linting issues

--- search.ts
+++ search.ts
@@ -2,36 +2,34 @@ import {Node} from "./nodes";
 
 //For more complex graph we could use A* search
 export function breadthFirstSearch(graph: Node[], start: Node, idToSearch: number): Node[] {
-  let queue: Node[] = [];
-  let path: Node[] = [];
+  const queue: Node[] = [];
+  const path: Node[] = [];
   start.path = new Array(start);
   queue.push(start);
   path.push(start);
-  let visited: number[] = [];
+  const visited: number[] = [];
   visited.push(start.id);
 
   while (queue.length > 0) {
-      let node = queue.shift();
+    const node = queue.shift();
+    if (node === undefined)
+      return [];
 
-      if (node === undefined)
-        return [];
+    for (let i = 0; i < node.connections.length; i++) {
+      const connectedNode = graph.find(e => e.id === node.connections[i]);
+      if (connectedNode === undefined)
+        continue;
 
-      for (var i = 0; i < node.connections.length; i++) {
-          let connectedNode = graph.find(e => e.id === node!.connections[i]);
-          if (connectedNode === undefined)
-            continue;
-
-          connectedNode.path = node.path === undefined ? [] : [...node.path];
-          connectedNode.path.push(connectedNode)
-          if (!visited.includes(connectedNode.id)) {
-              
-              if (connectedNode.id === idToSearch)
-                return connectedNode.path;
-              
-              visited.push(connectedNode.id);
-              queue.push(connectedNode);
-          }
+      connectedNode.path = node.path === undefined ? [] : [...node.path];
+      connectedNode.path.push(connectedNode)
+      if (!visited.includes(connectedNode.id)) {            
+        if (connectedNode.id === idToSearch)
+          return connectedNode.path;
+        
+        visited.push(connectedNode.id);
+        queue.push(connectedNode);
       }
+    }
   }
   return [];
-};
+}
