354f720d4794954b9f9d856f94299b3a5fc0bad1
Tommi Seppänen <tommi.t.seppanen@gmail.com>
2022-09-18 13:50:30 +0300

    Improve typing

--- search.ts
+++ search.ts
@@ -1,20 +1,20 @@
 import {Node} from "./nodes";
 
 //For more complex graph we could use A* search
-export function breadthFirstSearch(graph: Node[], start: Node, idToSearch: number) {
-    let queue : Node[] = [];
-    let path : Node[] = [];
+export function breadthFirstSearch(graph: Node[], start: Node, idToSearch: number): Node[] {
+    let queue: Node[] = [];
+    let path: Node[] = [];
     start.path = new Array(start);
     queue.push(start);
     path.push(start);
-    let visited : number[] = [];
+    let visited: number[] = [];
     visited.push(start.id);
   
     while (queue.length > 0) {
         let node = queue.shift();
   
         if (node === undefined)
-          return null;
+          return [];
   
         for (var i = 0; i < node.connections.length; i++) {
             let connectedNode = graph.find(e => e.id === node!.connections[i]);
@@ -33,5 +33,5 @@ export function breadthFirstSearch(graph: Node[], start: Node, idToSearch: numbe
             }
         }
     }
-    return null;
+    return [];
   };
