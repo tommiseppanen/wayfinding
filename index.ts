293d824ca20d6f1fde4c8d46af24011b19d84371
Tommi Seppänen <tommi.t.seppanen@gmail.com>
2022-09-18 12:43:36 +0300

    Move search to own file

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
-import {nodes, Node} from "./nodes";
+import {nodes} from "./nodes";
+import {breadthFirstSearch} from "./search";
 
 function initMap(): void {
 
@@ -7,9 +8,9 @@ function initMap(): void {
   window.mapInstance = new google.maps.Map(
       document.getElementById("map") as HTMLElement,
       {
-      center: { lat: 60.31689593036639, lng: 24.96863419739257 },
-      zoom: 17,
-      mapTypeId: "satellite",
+        center: { lat: 60.31689593036639, lng: 24.96863419739257 },
+        zoom: 17,
+        mapTypeId: "satellite",
       }
   );
 
@@ -20,8 +21,8 @@ function initMap(): void {
   };
 
   const endSymbol = {
-      path: google.maps.SymbolPath.FORWARD_OPEN_ARROW,
-      strokeColor: pathColor,
+    path: google.maps.SymbolPath.FORWARD_OPEN_ARROW,
+    strokeColor: pathColor,
   };
 
   window.path = new google.maps.Polyline({
@@ -46,12 +47,9 @@ function initMap(): void {
   
 function animateCircle(line: google.maps.Polyline) {
   let count = 0;
-
   window.setInterval(() => {
     count = (count + 1) % 400;
-
     const icons = line.get("icons");
-
     icons[1].offset = count / 4 + "%";
     line.set("icons", icons);
   }, 20);
@@ -81,42 +79,6 @@ function getNodes(start: number, end: number) {
   if (startNode)
     return breadthFirstSearch(nodes, startNode, end);
 }
-
-//For more complex graph we could use A* search
-function breadthFirstSearch(graph: Node[], start: Node, idToSearch: number) {
-  let queue : Node[] = [];
-  let path : Node[] = [];
-  start.path = new Array(start);
-  queue.push(start);
-  path.push(start);
-  let visited : number[] = [];
-  visited.push(start.id);
-
-  while (queue.length > 0) {
-      let node = queue.shift();
-
-      if (node === undefined)
-        return null;
-
-      for (var i = 0; i < node.connections.length; i++) {
-          let connectedNode = graph.find(e => e.id === node!.connections[i]);
-          if (connectedNode === undefined)
-            continue;
-
-          connectedNode.path = node.path === undefined ? [] : [...node.path];
-          connectedNode.path.push(connectedNode)
-          if (!visited.includes(connectedNode.id)) {
-              
-              if (connectedNode.id === idToSearch)
-                return connectedNode.path;
-              
-              visited.push(connectedNode.id);
-              queue.push(connectedNode);
-          }
-      }
-  }
-  return null;
-};
 
 window.initMap = initMap;
 window.find = find;
